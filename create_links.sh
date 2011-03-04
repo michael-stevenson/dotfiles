f8a4a73c5f8ed4ed16165f50a3e046ecc5737983
Michael Stevenson <michael.s.stevenson@gmail.com>
2011-03-04 15:35:34 -0600

    fix up link creation script

--- create_links.sh
+++ create_links.sh
@@ -11,7 +11,15 @@ DOTFILES=$(pwd)
 cd
 
 relink .vim          $DOTFILES/vim
-relink .vimrc        $DOTFILES/vim/vimrc
+relink .vimrc        $DOTFILES/.vimrc
+relink .gvimrc		 $DOTFILES/.gvimrc
+
+relink .screenrc	 $DOTFILES/.screenrc
+relink .tmux.conf	 $DOTFILES/.tmux.conf
+
+relink .zshrc		 $DOTFILES/.zshrc
+relink .zshenv		 $DOTFILES/.zshenv
+relink .zsh			 $DOTFILES/.zsh
 
 relink .gitconfig    $DOTFILES/.gitconfig
 relink .gitignore    $DOTFILES/.gitignore
